90b1a9ce967d3aed49c0e1d92f0d7055ad3b053c
vhzkclq0705 <vhzkclq0705@naver.com>
2025-06-09 09:49:30 +0000

    feat: Spark 작업 및 RDS 연결 설정

--- dags/voc_monthly_agg.py
+++ dags/voc_monthly_agg.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.python import ShortCircuitOperator, PythonOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
-from airflow.providers.google.cloud.operators.cloud_sql import CloudSQLExecuteQueryOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 
 # ----- 로깅 -----
 logging.basicConfig(
@@ -14,57 +14,67 @@ logging.basicConfig(
 logger = logging.getLogger("monthly_agg")
 
 # ----- 공통 클라이언트 -----
-BUCKET = "wh04-voc"
-BASE_PATH = "tmp/meta"
+BUCKET = "wh04-voc-bucket"
+BASE_PATH = "sample/raw/consulting_api_raw"
 
 # ----- 유틸 함수 -----
 def check_files_exist(**context):
-    from airflow.providers.google.cloud.hooks.gcs import GCSHook
+    from airflow.providers.amazon.aws.hooks.s3 import S3Hook
     year = context["logical_date"].year
     month = context["logical_date"].month
-    gcs_hook = GCSHook(gcp_conn_id="google_cloud_default")
-    logging.info(f"GCS Hook 연결 완료: {gcs_hook.project_id}")
+    s3_hook = S3Hook(aws_conn_id="aws_s3_conn")
+    logging.info(f"S3 연결 완료")
     
-    paths = [
+    # paths = [
         # f"{BASE_PATH}/consultings/year={year}/month={month}",
-        f"{BASE_PATH}/analysis_result/year={year}/month={month}"
-    ]
+    #     f"{BASE_PATH}/analysis_result/year={year}/month={month}"
+    # ]
 
-    for path in paths:
-        files = gcs_hook.list(bucket_name=BUCKET, prefix=path)
-        if not files:
-            return False
+    # for path in paths:
+    #     files = gcs_hook.list(bucket_name=BUCKET, prefix=BASE_PATH)
+    #     if not files:
+    #         return False
+    # return True
+    files = s3_hook.list_keys(bucket_name=BUCKET, prefix=BASE_PATH)
+    if not files:
+        return False
     return True
 
 def generate_sql(**context):
+    import uuid
     from io import BytesIO
     import pandas as pd
-    from airflow.providers.google.cloud.hooks.gcs import GCSHook
+    from airflow.providers.amazon.aws.hooks.s3 import S3Hook
     year = context["logical_date"].year
     month = context["logical_date"].month
-    gcs_hook = GCSHook(gcp_conn_id="google_cloud_default")
+    s3_hook = S3Hook(aws_conn_id="aws_s3_conn")
 
-    sql = []
-    for kind in ["category", "keyword", "sentiment"]:
-        path = f"{BASE_PATH}/{kind}/year={year}/month={month}"
-        files = gcs_hook.list(bucket_name=BUCKET, prefix=path)
+    # sql = []
+    # for kind in ["category", "keyword", "sentiment"]:
+    #     path = f"{BASE_PATH}/{kind}/year={year}/month={month}"
+    #     files = gcs_hook.list(bucket_name=BUCKET, prefix=path)
 
-        for obj in files:
-            data = gcs_hook.download(bucket_name=BUCKET, object_name=obj)
-            df = pd.read_parquet(BytesIO(data))
+    #     for obj in files:
+    #         data = gcs_hook.download(bucket_name=BUCKET, object_name=obj)
+    #         df = pd.read_parquet(BytesIO(data))
 
-            for _, raw in df.iterrows():
-                if kind == "sentiment":
-                    sql.append(f"""
-                    INSERT INTO sentiment_agg (analysis_reasult_id, positive, negative, year, month)
-                    VALUES ({raw['analysis_result_id']}, {raw['positive']}, {raw['negative']}, {year}, {month});
-                    """)
-                else:
-                    sql.append(f"""
-                    INSERT INTO {kind}_agg (analysis_result_id, {kind}, count, year, month)
-                    VALUES ({raw['analysis_result_id']}, '{row[kind]}', {raw['count']}, year, month);
-                    """)
-    return sql
+    #         for _, raw in df.iterrows():
+    #             if kind == "sentiment":
+    #                 sql.append(f"""
+    #                 INSERT INTO sentiment_agg (analysis_reasult_id, positive, negative, year, month)
+    #                 VALUES ({raw['analysis_result_id']}, {raw['positive']}, {raw['negative']}, {year}, {month});
+    #                 """)
+    #             else:
+    #                 sql.append(f"""
+    #                 INSERT INTO {kind}_agg (analysis_result_id, {kind}, count, year, month)
+    #                 VALUES ({raw['analysis_result_id']}, '{row[kind]}', {raw['count']}, year, month);
+    #                 """)
+    # return sql
+    return f"""
+    INSERT INTO sample (id)
+    VALUES ('{str(uuid.uuid4())}');
+    """
+
 
 # ----- DAG -----
 with DAG(
@@ -76,6 +86,8 @@ with DAG(
     start_date=datetime(2020, 1, 1),
     catchup=True,
     tags=["voc", "monthly", "agg"],
+    max_active_runs=3,
+    max_active_tasks=3
 ) as dag:
     check_data = ShortCircuitOperator(
         task_id="check_data",
@@ -85,8 +97,8 @@ with DAG(
 
     spark_agg = SparkSubmitOperator(
         task_id="spark_monthly_agg",
-        application="opt/airflow/spark_jobs/monthly_agg.py",
-        conn_id="spark_default",
+        application="/opt/spark_jobs/monthly_agg.py",
+        conn_id="spark_conn",
         application_args=[
             "--year", "{{ logical_date.year }}",
             "--month", "{{ '%02d' % logical_date.month }}",
@@ -103,10 +115,10 @@ with DAG(
         provide_context=True
     )
 
-    save_to_cloudsql = CloudSQLExecuteQueryOperator(
-        task_id="save_to_cloudsql",
-        gcp_cloudsql_conn_id="cloud_sql_postgres_conn",
+    save_to_rds = PostgresOperator(
+        task_id="save_to_rds",
+        postgres_conn_id="aws_postgres_conn",
         sql="{{ ti.xcom_pull(task_ids='prepare_sql') }}"
     )
 
-    check_data >> spark_agg >> prepare_sql >> save_to_cloudsql
+    check_data >> spark_agg >> prepare_sql >> save_to_rds
