f604880bb32cad3d77b61d6023482185d4d9e4fb
vhzkclq0705 <vhzkclq0705@naver.com>
2025-06-11 05:34:40 +0000

    feat: S3 경로 및 오타 수정

--- dags/voc_monthly_agg.py
+++ dags/voc_monthly_agg.py
@@ -15,7 +15,7 @@ logger = logging.getLogger("monthly_agg")
 
 # ----- 공통 클라이언트 -----
 BUCKET = "wh04-voc-bucket"
-BASE_PATH = "sample/raw/consulting_api_raw"
+BASE_PATH = "meta"
 
 # ----- 유틸 함수 -----
 def check_files_exist(**context):
@@ -24,24 +24,19 @@ def check_files_exist(**context):
     month = context["logical_date"].month
     s3_hook = S3Hook(aws_conn_id="aws_s3_conn")
     logging.info(f"S3 연결 완료")
-    
-    # paths = [
-        # f"{BASE_PATH}/consultings/year={year}/month={month}",
-    #     f"{BASE_PATH}/analysis_result/year={year}/month={month}"
-    # ]
 
-    # for path in paths:
-    #     files = gcs_hook.list(bucket_name=BUCKET, prefix=BASE_PATH)
-    #     if not files:
-    #         return False
-    # return True
-    files = s3_hook.list_keys(bucket_name=BUCKET, prefix=BASE_PATH)
-    if not files:
-        return False
+    paths = [
+        f"{BASE_PATH}/consultings/year={year}/month={month}",
+        f"{BASE_PATH}/analysis_result/year={year}/month={month}"
+    ]
+
+    for path in paths:
+        files = s3_hook.list_keys(bucket_name=BUCKET, prefix=path)
+        if not files:
+            return False
     return True
 
 def generate_sql(**context):
-    import uuid
     from io import BytesIO
     import pandas as pd
     from airflow.providers.amazon.aws.hooks.s3 import S3Hook
@@ -49,31 +44,21 @@ def generate_sql(**context):
     month = context["logical_date"].month
     s3_hook = S3Hook(aws_conn_id="aws_s3_conn")
 
-    # sql = []
-    # for kind in ["category", "keyword", "sentiment"]:
-    #     path = f"{BASE_PATH}/{kind}/year={year}/month={month}"
-    #     files = gcs_hook.list(bucket_name=BUCKET, prefix=path)
+    sql = []
+    for kind in ["category", "keyword"]:
+        path = f"{BASE_PATH}/agg/{kind}/year={year}/month={month}"
+        files = s3_hook.list_keys(bucket_name=BUCKET, prefix=path)
 
-    #     for obj in files:
-    #         data = gcs_hook.download(bucket_name=BUCKET, object_name=obj)
-    #         df = pd.read_parquet(BytesIO(data))
+        for obj in files:
+            data = s3_hook.download(bucket_name=BUCKET, object_name=obj)
+            df = pd.read_parquet(BytesIO(data))
 
-    #         for _, raw in df.iterrows():
-    #             if kind == "sentiment":
-    #                 sql.append(f"""
-    #                 INSERT INTO sentiment_agg (analysis_reasult_id, positive, negative, year, month)
-    #                 VALUES ({raw['analysis_result_id']}, {raw['positive']}, {raw['negative']}, {year}, {month});
-    #                 """)
-    #             else:
-    #                 sql.append(f"""
-    #                 INSERT INTO {kind}_agg (analysis_result_id, {kind}, count, year, month)
-    #                 VALUES ({raw['analysis_result_id']}, '{row[kind]}', {raw['count']}, year, month);
-    #                 """)
-    # return sql
-    return f"""
-    INSERT INTO sample (id)
-    VALUES ('{str(uuid.uuid4())}');
-    """
+            for _, raw in df.iterrows():
+                sql.append(f"""
+                INSERT INTO {kind}_agg ({kind}_agg_id, {kind}, count, year, month)
+                VALUES ('{raw['analysis_result_id']}', '{raw[kind]}', {raw['count']}, {year}, {month});
+                """)
+    return "\n".join(sql)
 
 
 # ----- DAG -----
@@ -86,8 +71,8 @@ with DAG(
     start_date=datetime(2020, 1, 1),
     catchup=True,
     tags=["voc", "monthly", "agg"],
-    max_active_runs=3,
-    max_active_tasks=3
+    max_active_runs=1,
+    max_active_tasks=1
 ) as dag:
     check_data = ShortCircuitOperator(
         task_id="check_data",
@@ -104,7 +89,7 @@ with DAG(
             "--month", "{{ '%02d' % logical_date.month }}",
             "--consult_path", f"{BASE_PATH}/consultings",
             "--result_path", f"{BASE_PATH}/analysis_result",
-            "--output_path", f"{BASE_PATH}/aggregated"
+            "--output_path", f"{BASE_PATH}/agg"
         ],
         verbose=True
     )
