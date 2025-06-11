fdc7e56f432791ae6676d7bc34074012782a1e49
vhzkclq0705 <vhzkclq0705@naver.com>
2025-06-11 07:17:39 +0000

    feat: Spark 작업 시, 변동된 데이터 모델링에 맞게 변경 및 주석 추가

--- spark_jobs/monthly_agg.py
+++ spark_jobs/monthly_agg.py
@@ -1,58 +1,46 @@
 import sys
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, explode, split, avg, count, lit
+from pyspark.sql.functions import col, explode, split, count, lit
 
 def main(year, month, consult_path, result_path, output_path):
+    """
+    Spark를 통해 카테고리 및 키워드 집계
+    
+    Args:  
+        year (int): 분석 년도  
+        month (int): 분석 월  
+        consult_path (str): 문의 데이터 경로  
+        result_path (str): 분석 결과 데이터 경로  
+        output_path (str): 저장 경로
+    """
     spark = SparkSession.builder.appName("VOC Monthly Aggregation").getOrCreate()
 
-    # 1. 데이터 불러오기
-    # consult_df = spark.read.parquet(f"{consult_path}/year={year}/month={month}")
+    # 데이터 불러오기
+    consult_df = spark.read.parquet(f"{consult_path}/year={year}/month={month}")
     result_df = spark.read.parquet(f"{result_path}/year={year}/month={month}")
 
-    # 2. 조인 (consulting_id 기준)
-    # df = result_df.join(
-    #     consult_df.select("consulting_id", "category"),
-    #     on="consulting_id",
-    #     how="inner"
-    # )
+    # 조인 (consulting_id 기준)
+    df = result_df.join(
+        consult_df.select("consulting_id", "category"),
+        on="consulting_id",
+        how="inner"
+    )
+    df = df.withColumn("year", lit(year)).withColumn("month", lit(month))
 
-    # # year, month 컬럼 추가
-    # df = df.withColumn("year", lit(year)).withColumn("month", lit(month))
+    # 카테고리별 집계
+    category_agg = df.groupBy("category", "year", "month").agg(
+        count("*").alias("count")
+    )
 
-    # # 3. 카테고리별 집계
-    # category_agg = df.groupBy("category", "year", "month").agg(
-    #     count("*").alias("count")
-    # )
-
-    # # 4. 키워드별 집계
-    # df_keywords = df.withColumn("keyword", explode(split(col("keywords"), ",")))
-    # keyword_agg = df_keywords.groupBy("keyword", "year", "month").agg(
-    #     count("*").alias("count")
-    # )
-
-    # # 5. 전체 긍부정 평균
-    # sentiment_agg = df.groupBy("year", "month").agg(
-    #     avg("positive").alias("avg_positive"),
-    #     avg("negative").alias("avg_negative")
-    # )
-
-    # # 6. 저장
-    # category_agg.write.mode("overwrite").partitionBy("year", "month").parquet(f"{output_path}/category")
-    # keyword_agg.write.mode("overwrite").partitionBy("year", "month").parquet(f"{output_path}/keyword")
-    # sentiment_agg.write.mode("overwrite").partitionBy("year", "month").parquet(f"{output_path}/sentiment")
-    
-    # ---- 테스트용 -----
-    result_df = result_df.withColumn("year", lit(year)).withColumn("month", lit(month))
-    df_keywords = result_df.withColumn("keyword", explode(split(col("keywords"), ",")))
+    # 키워드별 집계
+    df_keywords = df.withColumn("keyword", explode(split(col("keywords"), ",")))
     keyword_agg = df_keywords.groupBy("keyword", "year", "month").agg(
         count("*").alias("count")
     )
-    sentiment_agg = result_df.groupBy("year", "month").agg(
-        avg("positive").alias("avg_positive"),
-        avg("negative").alias("avg_negative")
-    )
+
+    # 저장
+    category_agg.write.mode("overwrite").partitionBy("year", "month").parquet(f"{output_path}/category")
     keyword_agg.write.mode("overwrite").partitionBy("year", "month").parquet(f"{output_path}/keyword")
-    sentiment_agg.write.mode("overwrite").partitionBy("year", "month").parquet(f"{output_path}/sentiment")
 
     spark.stop()
 
